48836a3d65a47672bf75a3f44acd45708da07df6
Velshh <pkot2003@gmail.com>
2025-03-21 19:35:45 +0100

    Initial commit

---
+++ src/main/java/me/mmtr/koala/KoalaApplication.java
@@ -0,0 +1,13 @@
+package me.mmtr.koala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoalaApplication.class, args);
+    }
+
+}

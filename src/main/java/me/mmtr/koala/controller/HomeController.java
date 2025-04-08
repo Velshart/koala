a7fa3e7105cdecf1764d948a05bb6d2086b221a0
Velshh <pkot2003@gmail.com>
2025-04-08 19:14:32 +0200

    add a list of created articles to the homepage

--- src/main/java/me/mmtr/koala/controller/HomeController.java
+++ src/main/java/me/mmtr/koala/controller/HomeController.java
@@ -1,20 +1,26 @@
 package me.mmtr.koala.controller;
 
 import jakarta.servlet.http.HttpSession;
+import me.mmtr.koala.data.Article;
 import me.mmtr.koala.data.User;
 import me.mmtr.koala.repository.UserRepository;
+import me.mmtr.koala.repository.dao.ArticleDAO;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
     private final UserRepository userRepository;
+    private final ArticleDAO articleDAO;
 
-    public HomeController(UserRepository userRepository) {
+    public HomeController(UserRepository userRepository, ArticleDAO articleDAO) {
         this.userRepository = userRepository;
+        this.articleDAO = articleDAO;
     }
 
     @GetMapping("/home")
@@ -23,9 +29,15 @@ public class HomeController {
 
         String email = principal.getAttribute("email");
         User user = userRepository.findByEmail(email).orElseThrow();
-        session.setAttribute("principalUser", user);
+        List<Article> userArticles = articleDAO.findAll()
+                .stream()
+                        .filter(article -> article.getAuthor().equals(user.getName()))
+                                .toList();
 
+        session.setAttribute("principalUser", user);
+        model.addAttribute("userArticles", userArticles);
         model.addAttribute("user", user);
+
         return "home";
     }
 }

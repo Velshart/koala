1233720cf1516931a86b0a981dec364e4a5b14ed
Velshh <pkot2003@gmail.com>
2025-04-14 17:24:26 +0200

    add profile page

--- src/main/java/me/mmtr/koala/controller/ApplicationMainController.java
+++ src/main/java/me/mmtr/koala/controller/ApplicationMainController.java
@@ -11,14 +11,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class HomeController {
+public class ApplicationMainController {
     private final UserRepository userRepository;
     private final ArticleDAO articleDAO;
 
-    public HomeController(UserRepository userRepository, ArticleDAO articleDAO) {
+    public ApplicationMainController(UserRepository userRepository, ArticleDAO articleDAO) {
         this.userRepository = userRepository;
         this.articleDAO = articleDAO;
     }
@@ -28,16 +29,31 @@ public class HomeController {
         OAuth2User principal = authenticationToken.getPrincipal();
 
         String email = principal.getAttribute("email");
-        User user = userRepository.findByEmail(email).orElseThrow();
-        List<Article> userArticles = articleDAO.findAll()
-                .stream()
-                        .filter(article -> article.getAuthor().equals(user.getName()))
-                                .toList();
-
+        User user = userRepository.findByEmail(email).orElse(null);
+        List<Article> userArticles;
+        if (user != null) {
+             userArticles = articleDAO.findAll()
+                    .stream()
+                    .filter(article -> article.getAuthor().equals(user.getName()))
+                    .toList();
+        }else {
+            userArticles = new ArrayList<>();
+        }
         session.setAttribute("principalUser", user);
+        session.setAttribute("authTokenPrincipal", authenticationToken.getPrincipal());
         model.addAttribute("userArticles", userArticles);
         model.addAttribute("user", user);
-
         return "home";
+    }
+
+    @GetMapping("/profile")
+    public String profile(HttpSession session, Model model) {
+        OAuth2User principal = (OAuth2User) session.getAttribute("authTokenPrincipal");
+
+        model.addAttribute("authTokenPrincipalName", principal.getAttribute("name"));
+        model.addAttribute("authTokenPrincipalEmail", principal.getAttribute("email"));
+        model.addAttribute("authTokenPrincipalPicture", principal.getAttribute("picture"));
+
+        return "profile";
     }
 }
